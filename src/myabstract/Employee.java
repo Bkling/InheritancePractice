3a38bd0452ab5b61f388e1cd687b848b10469fda
Benjamin <Benjamin@Toshiba-Ben>
2013-01-26 20:05:44 -0600

    Stopped working on this, because I am having trouble with my Abstract package.

---
+++ src/myabstract/Employee.java
@@ -0,0 +1,8 @@
+package myabstract;
+
+public abstract class Employee {
+
+    public abstract void getWage();
+
+    public abstract void getAge();
+}
---
+++ src/myabstract/SalariedEmployee.java
@@ -0,0 +1,52 @@
+package myabstract;
+
+public class SalariedEmployee extends Employee {
+
+    private String firstName;
+    private String lastName;
+    private String Ssn;
+    private int age;
+    private double wage;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getSsn() {
+        return Ssn;
+    }
+
+    public void setSsn(String Ssn) {
+        this.Ssn = Ssn;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setWage(double wage) {
+        this.wage = wage;
+    }
+
+    @Override
+    public void getWage() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void getAge() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+}
